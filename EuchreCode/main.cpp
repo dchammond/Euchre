2a4f270ba6279b5b344e262ada3355cf809d73d0
icehockey38 <dillonhammond@gmail.com>
2014-09-12 15:53:52 -0500

    Added loadShaders and createWindow

--- EuchreCode/main.cpp
+++ EuchreCode/main.cpp
@@ -12,6 +12,80 @@
 #include "SOIL.h"
 #define GLM_FORCE_RADIANS
 #include "glm.hpp"
+#include <iostream>
+#include <fstream>
+
+GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path) {
+ 
+	// Create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	// Read the Vertex Shader code from the file
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream;
+	VertexShaderStream.open(vertex_file_path);
+	if(VertexShaderStream.is_open())
+	{
+		std::string Line = "";
+		while(getline(VertexShaderStream, Line))
+			VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+ 
+	// Read the Fragment Shader code from the file
+	std::string FragmentShaderCode;
+	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+	if(FragmentShaderStream.is_open()){
+		std::string Line = "";
+		while(getline(FragmentShaderStream, Line))
+			FragmentShaderCode += "\n" + Line;
+		FragmentShaderStream.close();
+	}
+	GLint Result;
+	// Compile Vertex Shader
+	char const * VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+	glCompileShader(VertexShaderID);
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	//	if (Result == GL_TRUE) {
+	//		cout << "NOOO" << endl;
+	//		cout << VertexSourcePointer << endl;
+	//	}
+	
+ 
+	// Compile Fragment Shader
+	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+	glCompileShader(FragmentShaderID);
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	//	if (Result == GL_TRUE) {
+	//		cout << "NOOO" << endl;
+	//		cout << FragmentSourcePointer << endl;
+	//	}
+	
+ 
+	// Link the program
+	GLuint ProgramID = glCreateProgram();
+	glAttachShader(ProgramID, VertexShaderID);
+	glAttachShader(ProgramID, FragmentShaderID);
+	glLinkProgram(ProgramID);
+ 
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+ 
+	return ProgramID;
+}
+
+SDL_Window* createWindow(const char* title, int x, int y, int w, int h, Uint32 flags) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_Window *window = SDL_CreateWindow(title, x, y, w, h, flags);
+	return window;
+}
 
 int main() {
 	return 0;
